423d32462d289eba3f09663893afed74e3c96640
Muhamad Nur Alim <82062969+Mnuralim@users.noreply.github.com>
2025-06-10 10:50:13 +0800

    Update rpc

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const PHAROS_CONFIG = {
   RPC_URL:
-    "https://api.zan.top/node/v1/pharos/testnet/1761472bf26745488907477d23719fb5",
+    "https://testnet.dplabs-internal.com",
   WETH_ADDRESS: "0x76aaada469d23216be5f7c596fa25f282ff9b364",
   API_BASE_URL: "https://api.pharosnetwork.xyz",
   EXPLORER_URL: "https://testnet.pharosscan.xyz/tx",
